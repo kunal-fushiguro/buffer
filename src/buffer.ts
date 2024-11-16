bd13a882681f22327f59c2554bd99685cc5f8407
kunal-fushiguro <natwarlaluzumaki@gmail.com>
2024-11-16 15:53:12 +0530

    buffer

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,1 +1,7 @@
-console.log("heelo");
+import { Buffer } from "buffer";
+
+const memoryContainer = Buffer.alloc(4); // 4 bytes
+
+memoryContainer[0] = 0xf4;
+console.log(memoryContainer);
+console.log(memoryContainer[0]);
